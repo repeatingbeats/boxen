1239f64841d94a2b33de63081b3be228f4cc38e6
Steve Lloyd <steve@repeatingbeats.com>
2013-03-01 12:00:06 -0800

    Allow OS X Lion to see what breaks

--- lib/boxen/preflight/os.rb
+++ lib/boxen/preflight/os.rb
@@ -2,7 +2,8 @@ require "boxen/preflight"
 
 class Boxen::Preflight::OS < Boxen::Preflight
   def ok?
-    `sw_vers -productVersion`.start_with? "10.8"
+    # Let's see what breaks on 10.7
+    Float(`sw_vers -productVersion`.match('\A[\d]+\.[\d]')[0]) >= 10.7
   end
 
   def run
